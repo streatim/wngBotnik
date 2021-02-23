3e5b49b34bb5c1205af5ac7d3d71bafb2c37e078
Tim het Maus <streatim@umich.edu>
2021-02-23 17:55:31 -0500

    Adds intValCheck function

--- common.js
+++ common.js
@@ -5,6 +5,11 @@ const common = {
     return output;
   },
 
+  intValCheck(value, def){
+    output = (isNaN(parseInt(value))) ? def : parseInt(value);
+    return output;
+  },
+
   badCall(type) {
     //Return a "bad call" message that lets the person know they used the command !wg(type) wrong.
     return 'Please see !wghelp '+type+' for instructions on how to use the !wg'+type+' command.';
