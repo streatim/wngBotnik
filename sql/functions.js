a4b2b794c0af6a8047da9d1af960a394db746961
Tim het Maus <streatim@umich.edu>
2021-03-14 17:54:27 -0400

    Add SQL scripts.

---
+++ sql/functions.js
@@ -0,0 +1,24 @@
+const { db } = require('./auth.json');
+const { Sequelize } = require('sequelize');
+
+const sql = {
+    connection : new Sequelize(db.db, db.un, db.pw, {
+        host: 'localhost',
+        dialect: 'mariadb',
+        dialectOptions: {
+            // Mariadb options here
+            connectTimeout: 1000
+        }
+    }),
+
+    async test (db) {
+        try {
+            await db.authenticate();
+            return 'TRUE';
+        } catch (error) {
+            return error;
+        }
+    }
+}
+
+module.exports = sql;
